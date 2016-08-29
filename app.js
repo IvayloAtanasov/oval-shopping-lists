b9d3b557a08abf225e26fd6add20e3b5b7bd5bfc
Ivaylo Atanasov <aivo@devlabs.bg>
2016-08-29 22:49:34 +0300

    Added express server and milligram css library.

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const dbConfig = require('./knexfile').development;
 const knex = require('knex')(dbConfig);
 
-console.log(knex);
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
 
+app.use(express.static('public'));
 
-console.log('Hey, Im working ...almost');
+// parse request params into req.body
+app.use(bodyParser.urlencoded({
+	// type is default but who cares
+	type: 'application/x-www-form-urlencoded',
+	extended: true
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(8080, () => console.log('API listening on port 8080'));

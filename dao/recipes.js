aeb919dde2e9676ab4b4f005c2dfa9ebe927b99d
Ivaylo Atanasov <aivo@devlabs.bg>
2016-09-20 20:35:03 +0300

    Added recipe delete route. Moved recipe insert logic into recipe dao.

--- dao/recipes.js
+++ dao/recipes.js
@@ -1,4 +1,3 @@
-const knex = require('knex');
 const Base = require('./base');
 
 class Recipes extends Base {
@@ -11,6 +10,7 @@ class Recipes extends Base {
     // Adds category name as well as category id.
     // Recipes have 1 category (for now).
     joinCategories() {
+        const db = this.db;
         this.query
             .select(
                 `${this.table}.id as id`,
@@ -20,7 +20,7 @@ class Recipes extends Base {
             .leftJoin('categorisables', function() {
                 // Note: 'this' here refers to categorisables table
                 this.on('recipes.id', '=', 'categorisables.categorisable_id')
-                    .on(knex.raw("'recipes' = categorisables.categorisable_type"))
+                    .on(db.raw("'recipes' = categorisables.categorisable_type"))
             })
             .leftJoin('categories', 'categorisables.category_id', 'categories.id');
 
@@ -41,6 +41,135 @@ class Recipes extends Base {
             .leftJoin('products', 'product_recipes.product_id', 'products.id');
 
             return this;
+    }
+
+    insert(recipeName, recipeDescription, minutesToCook, category, products) {
+        return this.db.transaction(trx => {
+
+            // insert recipe
+            return trx
+                .insert({
+                    name: recipeName, 
+                    description: recipeDescription, 
+                    minutes_to_cook: minutesToCook
+                }, 'id')
+                .into(this.table)
+                .then(recipeId => {
+                    recipeId = recipeId[0];
+                    //console.log('inserted recipeID', recipeId);
+                    // insert category
+                    return new Promise((resolve, reject) => {
+                        // link recipe to category in pivot table
+                        // returns categoryId in resolver
+                        function setRecipeCategory(recipeId, categoryId) {
+                            return trx
+                                .insert({
+                                    categorisable_id: recipeId,
+                                    categorisable_type: 'recipes',
+                                    category_id: categoryId
+                                })
+                                .into('categorisables')
+                                .then(() => categoryId);
+                        }
+
+                        const categoryId = category.id;
+                        if (categoryId) {
+                            return setRecipeCategory(recipeId, categoryId)
+                                .then(resolve)
+                                .catch(reject);
+                        } else {
+                            // no such category, create one, then insert
+                            return trx
+                                .insert({name: category.name}, 'id')
+                                .into('categories')
+                                .then(categoryId => {
+                                    return setRecipeCategory(recipeId, categoryId);
+                                })
+                                .then(resolve)
+                                .catch(reject);
+                        }
+                    })
+                    .then(categoryId => {
+                        //console.log('inserted categoryId', categoryId);
+
+                        function setRecipeProduct(recipeId, productId, quantity) {
+                            return trx
+                                .insert({
+                                    product_id: productId,
+                                    recipe_id: recipeId,
+                                    quantity: quantity
+                                })
+                                .into('product_recipes')
+                                .then(() => productId);
+                        }
+
+                        let productInsertPromises = [];
+                        products.forEach(product => {
+                            let productPromise = new Promise((resolve, reject) => {
+                                const productId = product.id;
+                                if (productId) {
+                                    return setRecipeProduct(recipeId, productId, product.qty)
+                                    .then(resolve)
+                                    .catch(reject);
+                                } else {
+                                    // no such product, create one then set to this recipe
+                                    return trx
+                                        .insert({name: product.name}, 'id')
+                                        .into('products')
+                                        .then(productId => {
+                                            return setRecipeProduct(recipeId, productId, product.qty);
+                                        })
+                                        .then(resolve)
+                                        .catch(reject);
+                                }
+                            });
+
+                            productInsertPromises.push(productPromise);
+                        });
+
+                        return Promise.all(productInsertPromises)
+                            .then(productIds => {
+                                //console.log(`inserted ${productIds.length} products`);
+
+                                return 'All records inserted';
+                            });
+                    });
+                });
+        });
+    }
+
+    delete(recipeId) {
+        return this.db.transaction(trx => {
+            // detach products - delete from product_recipes
+            return trx
+                .del()
+                .from('product_recipes')
+                .where('recipe_id', recipeId)
+                .then(deletedCount => {
+                    console.log(`Deleted ${deletedCount} records from product recipes.`);
+                    // detach category - delete from categorisables
+                    return trx
+                        .del()
+                        .from('categorisables')
+                        .where({
+                            categorisable_id: recipeId,
+                            categorisable_type: 'recipes'
+                        })
+                        .then(deletedCount => console.log(`Detached ${deletedCount} categories from recipe.`));
+                })
+                .then(() => {
+                    // delete recipe
+                    return trx
+                        .del()
+                        .from(this.table)
+                        .where('id', recipeId)
+                        .then(deletedCount => console.log(`Deleted ${deletedCount} recipe`));
+                });
+        });
+    }
+
+    update(recipeId, recipeName, recipeDescription, minutesToCook, category, products) {
+
     }
 
 }
--- index.js
+++ index.js
@@ -149,127 +149,110 @@ function compactRecipesIntoJson(records) {
     return recipesList;
 }
 
+/**
+ * Request body:
+ * { 
+ *     recipeName: String, 
+ *     recipeDescription: String, 
+ *     minutesToCook: Number,
+ *     category: {
+ *         id: Number [Optional],
+ *         name: String [Optional]
+ *     }, 
+ *     products: [{
+ *         id: Number [Optional],
+ *         name: String [Optional],
+ *         qty: String
+ *     }]
+ * }
+ *
+ * Response body:
+ * {
+ *     msg: String
+ * }
+ */
 app.post('/api/recipes', (req, res) => {
-    let { recipeName, recipeDescription, minutesToCook,
+    const { recipeName, recipeDescription, minutesToCook,
         category, products} = req.body;
 
     // TODO: params validation
 
     //console.log( recipeName, recipeDescription, minutesToCook, category, products);
 
-    knex.transaction(trx => {
-
-        // insert recipe
-        return trx
-            .insert({
-                name: recipeName, 
-                description: recipeDescription, 
-                minutes_to_cook: minutesToCook
-            }, 'id')
-            .into('recipes')
-            .then(recipeId => {
-                recipeId = recipeId[0];
-                console.log('inserted recipeID', recipeId);
-                // insert category
-                return new Promise((resolve, reject) => {
-                    // link recipe to category in pivot table
-                    // returns categoryId in resolver
-                    function setRecipeCategory(recipeId, categoryId) {
-                        return trx
-                            .insert({
-                                categorisable_id: recipeId,
-                                categorisable_type: 'recipes',
-                                category_id: categoryId
-                            })
-                            .into('categorisables')
-                            .then(() => categoryId);
-                    }
-
-                    const categoryId = category.id;
-                    if (categoryId) {
-                        return setRecipeCategory(recipeId, categoryId)
-                            .then(resolve)
-                            .catch(reject);
-                    } else {
-                        // no such category, create one, then insert
-                        return trx
-                            .insert({name: category.name}, 'id')
-                            .into('categories')
-                            .then(categoryId => {
-                                return setRecipeCategory(recipeId, categoryId);
-                            })
-                            .then(resolve)
-                            .catch(reject);
-                    }
-                })
-                .then(categoryId => {
-                    console.log('inserted categoryId', categoryId);
-
-                    function setRecipeProduct(recipeId, productId, quantity) {
-                        return trx
-                            .insert({
-                                product_id: productId,
-                                recipe_id: recipeId,
-                                quantity: quantity
-                            })
-                            .into('product_recipes')
-                            .then(() => productId);
-                    }
-
-                    let productInsertPromises = [];
-                    products.forEach(product => {
-                        let productPromise = new Promise((resolve, reject) => {
-                            const productId = product.id;
-                            if (productId) {
-                                return setRecipeProduct(recipeId, productId, product.qty)
-                                .then(resolve)
-                                .catch(reject);
-                            } else {
-                                // no such product, create one then set to this recipe
-                                return trx
-                                    .insert({name: product.name}, 'id')
-                                    .into('products')
-                                    .then(productId => {
-                                        return setRecipeProduct(recipeId, productId, product.qty);
-                                    })
-                                    .then(resolve)
-                                    .catch(reject);
-                            }
-                        });
-
-                        productInsertPromises.push(productPromise);
-                    });
-
-                    return Promise.all(productInsertPromises)
-                        .then(productIds => {
-                            console.log(`inserted ${productIds.length} products`);
-
-                            return 'All records inserted';
-                        });
+    recipes
+        .insert(recipeName, recipeDescription, minutesToCook, category, products)
+        .then(successMsg => {
+            console.log(successMsg);
+            // TODO: how to return the new recipe here? + new products if any
+            res
+                .status(200)
+                .json({
+                    msg: successMsg
                 });
-            })
-    })
-    .then(successMsg => {
-        console.log(successMsg);
-        // TODO: how to return the new recipe here? + new products if any
-        res
-            .status(200)
-            .json({
-                msg: successMsg
-            });
-    })
-    .catch(err => {
-        console.log(err);
-        res
-            .status(500)
-            .json({
-                error: err
-            });
-    });
+        })
+        .catch(err => {
+            console.log(err);
+            res
+                .status(500)
+                .json({
+                    error: err
+                });
+        });
 });
 
-app.put('/api/recipes', (req, res) => {
-    // TODO: edit recipe. Reuse from post recipe
+app.put('/api/recipes/:id', (req, res) => {
+    const recipeId = req.params.id;
+    const { recipeName, recipeDescription, minutesToCook,
+        category, products} = req.body;
+
+    // TODO: params validation
+
+    recipes
+        .update(recipeId, recipeName, recipeDescription, minutesToCook, category, products)
+        .then(successMsg => {
+            console.log(successMsg);
+            // TODO: how to return the updated recipe here? + new products if any
+            res
+                .status(200)
+                .json({
+                    msg: successMsg
+                });
+        })
+        .catch(err => {
+            console.log(err);
+            res
+                .status(500)
+                .json({
+                    error: err
+                });
+        });
+});
+
+
+/**
+ * Request body:
+ * {}
+ *
+ * Response body:
+ * {}
+ */
+app.delete('/api/recipes/:id', (req, res) => {
+    const recipeId = req.params.id;
+    // seek and destroy
+    recipes
+        .delete(recipeId)
+        .then(() => {
+            res
+                .status(200)
+                .json({});
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({
+                    error: err
+                });
+        });
 });
 
 app.get('/api/categories', (req, res) => {
